1ce7318df4e61c1065d7b907fafea3f285b51de2
Nikitha Nagaraj <nikithanagaraj23@gmail.com>
2018-03-15 14:28:14 -0400

    Initial Project set up

---
+++ Project1a/turbo/lib/turbo/application.ex
@@ -0,0 +1,29 @@
+defmodule Turbo.Application do
+  use Application
+
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Start the endpoint when the application starts
+      supervisor(TurboWeb.Endpoint, []),
+      # Start your own worker by calling: Turbo.Worker.start_link(arg1, arg2, arg3)
+      # worker(Turbo.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Turbo.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    TurboWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
